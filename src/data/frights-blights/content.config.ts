f0d5a1c3d942efadc331a1aaaa3c2549d90ed6a6
William Cowan <william@modocosm.io>
2025-05-12 08:15:08 +0530

    Frights and Blights: Start

--- src/data/frights-blights/content.config.ts
+++ src/data/frights-blights/content.config.ts
@@ -1,19 +1,23 @@
 import { defineCollection, z } from 'astro:content';
 import { glob, file } from 'astro/loaders'; 
 
+
 const jt = {
-  z: {                                  // enums, re-used objects, etc.
+  z: {                                   // enums, re-used objects, etc.
     stats: z.tuple([                    // [ perceived, essential ] or [ bounded, unbounded ]
-      z.number().int().min(-8).max(8),  // -10 to 10 (starts -8 to 8)
-      z.number().int().min(-8).max(8)   // -10 to 10 (starts -8 to 8)
+      z.number().int().min(-10).max(10),  // -10 to 10 (starts -8 to 8)
+      z.number().int().min(-10).max(10)   // -10 to 10 (starts -8 to 8)
     ]),
-    reliances: z.object({             
-      primary: z.array(               // +/- 5 to roll on these stats
-        z.string()
-      ).length(2),
-      secondary: z.array(             // +/- 2 to roll on these stats
-        z.string()
-      ).length(2)
+    reliances: z.object({
+      // select 2 statS
+      primary: z.array(z.enum([
+        "allure","aural","feel","legerdemain","remembrance","resistance","sight","touch",
+        "consumption","focus","imagination","madness","magic","might","shield","skill"
+      ])).length(2),
+      secondary: z.array(z.enum([
+        "allure","aural","feel","legerdemain","remembrance","resistance","sight","touch",
+        "consumption","focus","imagination","madness","magic","might","shield","skill"
+      ])).length(2),
     }),
     die: z.enum([
       "d4", "d6", "d8", "d10", "d12", "d20",  // standard dice
@@ -21,7 +25,7 @@ const jt = {
       // common uses of standard dice to create emulate sides that are not possible with standard dice
       "d3",                   // d6 / 2 = 1&4 = 1, 3&5 = 2, 3&6 = 3 OR d6 / 2 round up
       "d%",                   // d100 OR 2d10 where 1st die is tens, 2nd die is ones
-      "d3-1"                 // d6 from the Betrayal Series --> sides: [0, 0, 1, 1, 2, 2]
+      "d3-1"                  // d6 from the Betrayal Series --> sides: [0, 0, 1, 1, 2, 2]
     ])
   }
 }
@@ -31,7 +35,7 @@ const character = defineCollection({
     player: z.string(),                     // player name
     name: z.array(z.string()).length(3),    // [first, middle, last]
     personificiation: z.string(),
-    level: z.number().int().min(0).max(5),
+    level: z.number().int().min(0).max(5),   
     hunger: z.number().int(),                // min? max?
     health: z.number().int().positive(),
     perception: z.object({
@@ -55,15 +59,14 @@ const character = defineCollection({
       skill:       jt.z.stats
     }),
     lingering: z.array(z.object({
-      key: z.string(),                          // perception.['$key'][1] 
-      value: z.number(),                        // value stored from the roll,
-      uses: z.array(z.boolean()).min(1).max(3)  // 1-3 boolean values, based on d6 roll as d3 (1,1,2,2,3,3)
-    })),                                        // every use convert 1 false to true, when all true REMOVE
+      key: z.string(),                                // perception.['$key'][1] 
+      value: z.number(),                              // value stored from the roll,
+      uses: z.array(z.boolean()).min(1).max(3)        // 1-3 boolean values, based on d6 roll as d3 (1,1,2,2,3,3)
+    })).max(3),                                       // every use convert 1 false to true, when all true REMOVE
     languages: z.array(z.string()).max(5),
     mastery: z.array(z.string()).max(4),
     class: z.object({
-      name: z.enum([
-        
+      name: z.enum(["brawler", "devout", "cultist", "sensor", "mage", "healer", "mystic", "thief", "appeaser", "jester", "knight", "gorger", "meditator", "swordsmaster", "archer", "peasant"]), 
       reliances: jt.z.reliances
     }),
     blessed: z.boolean(),
